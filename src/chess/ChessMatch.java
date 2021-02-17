cbd172d55ead200a60e4258a7ca89a29b9f746b2
Eduardo Bouhid <eduardobouhid.neto@gmail.com>
2021-02-17 10:05:03 -0300

    Piece placement added

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,13 +11,14 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] matPieces = new ChessPiece[board.getRows()][board.getColumns()];
 
-		for (int i = 1; i < board.getRows(); i++) {
-			for (int j = 1; j < board.getColumns(); j++) {
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
 				matPieces[i][j] = (ChessPiece) board.piece(i, j);
 			}
 		}
@@ -22,5 +26,11 @@ public class ChessMatch {
 		return matPieces;
 	}
 	
+	private void initialSetup() {
+		//Coloca as peças na posição inicial
+		//TODO testando apenas 3 peças
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1) );
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 3));
+	}
 	
 }
