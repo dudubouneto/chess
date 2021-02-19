a3ba904a5097136175a1bdf8c0217fea2bd6f095
Eduardo Bouhid <eduardobouhid.neto@gmail.com>
2021-02-19 14:29:49 -0300

    UI improvement - colored board

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,12 +3,18 @@ package application;
 import chess.ChessMatch;
 
 public class Program {
-
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RESET = "\u001B[0m";
+	
 	public static void main(String[] args) {
 		ChessMatch chessMatch = new ChessMatch();
-		
+
+		System.out.print(ANSI_BLACK_BACKGROUND);
+
 		UI.printBoard(chessMatch.getPieces());
-		
+
+		// Voltando terminal pra cor normal
+		System.out.print(ANSI_RESET);
 	}
 
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -1,30 +1,55 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
 
-	
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+	public static final String ANSI_RESET = "\u001B[0m" + ANSI_BLACK_BACKGROUND;
+
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
-			System.out.print("- ");
+			System.out.print(ANSI_WHITE + "-" + ANSI_RESET);
+		} else {
+			if (piece.getColor() == Color.WHITE) {
+				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+			} else {
+				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+			}
 		}
-		else {
-			System.out.print(piece + " ");
-		}
+
+		System.out.print(" ");
 	}
-	
-	public static void printBoard (ChessPiece[][] pieceMat) {
+
+	public static void printBoard(ChessPiece[][] pieceMat) {
 		for (int i = 0; i < pieceMat.length; i++) {
-			//Printando a margem do tabuleiro
-			System.out.print(8 - i + " ");
+			// Printando a margem do tabuleiro
+			System.out.print(ANSI_WHITE + (8 - i) + " " + ANSI_RESET);
 			for (int j = 0; j < pieceMat.length; j++) {
 				printPiece(pieceMat[i][j]);
 			}
-			//Pulando linha
+			// Pulando linha
 			System.out.println();
 		}
-		//Margem inferior
-		System.out.println("  a b c d e f g h");
+		// Margem inferior
+		System.out.println(ANSI_WHITE + "  a b c d e f g h" + ANSI_RESET);
 	}
 }
